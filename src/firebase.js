9398e176d68921edc29d89bd0385f6a5e591b805
Amina0426 <amublue26@gmail.com>
2025-10-10 12:56:25 +0530

    secured

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAf5jC0m7QcqEX6-PMh69Nff08TlPp-Ai8",
-  authDomain: "gm-associates-website.firebaseapp.com",
-  projectId: "gm-associates-website",
-  storageBucket: "gm-associates-website.firebasestorage.app",
-  messagingSenderId: "10760112919",
-  appId: "1:10760112919:web:599275a9e089d5b2694552",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
